c305fdc0215228142d3f378d55b38f5cf158e8d9
Tinashe Kupfuwa <tinashekupfuwa@Tinashes-MacBook-Air.local>
2025-02-21 04:45:37 +0200

    initial push and final push

---
+++ app.js
@@ -0,0 +1,19 @@
+const incrementButton = document.querySelector(".increment");
+const decrementButton = document.querySelector(".decrement");
+let counterDigit = document.querySelector(".counter");
+
+let count = 0;
+
+incrementButton.addEventListener("click", () => {
+  if (count < 10) {
+    count++;
+    counterDigit.innerText = count;
+  }
+});
+
+decrementButton.addEventListener("click", () => {
+  if (count > 1) {
+    count = count - 1;
+    counterDigit.innerText = count;
+  }
+});
