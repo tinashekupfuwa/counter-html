2423b4e5d339dbb943332dca4bbb16bbeb348e88
Tinashe Kupfuwa <tinashekupfuwa@Tinashes-MacBook-Air.local>
2025-02-21 12:52:44 +0200

    add a simpler way of incrementing and decrementing without using an if statement and a count variable

--- app.js
+++ app.js
@@ -4,16 +4,26 @@ let counterDigit = document.querySelector(".counter");
 
 let count = 0;
 
-incrementButton.addEventListener("click", () => {
-  if (count < 10) {
-    count++;
-    counterDigit.innerText = count;
-  }
-});
+// incrementButton.addEventListener("click", () => {
+//   if (count < 10) {
+//     count++;
+//     counterDigit.innerText = count;
+//   }
+// });
 
 decrementButton.addEventListener("click", () => {
   if (count > 1) {
     count = count - 1;
     counterDigit.innerText = count;
   }
+});
+
+incrementButton.addEventListener("click", () => {
+  let currentCount = parseInt(counterDigit.innerText) || 0;
+  counterDigit.innerText = currentCount + 1;
+});
+
+decrementButton.addEventListener("click", () => {
+  let currentCount = parseInt(counterDigit.innerText) || 0;
+  counterDigit.innerText = currentCount - 1;
 });
